afbfd3e4efe3087cf2519aef334fa9f1562843a8
janiecacastro <54694263+janiecacastro@users.noreply.github.com>
2019-12-18 19:29:54 -0500

    Update self.js

--- mmp210/self.js
+++ mmp210/self.js
@@ -8,8 +8,8 @@ function draw() {
   background(220);
  
  stroke(0);
-  for (var x = 0; x <= mouseX; x += 50) {
-    for (var y = 0; y <= mouseY; y += 50){
+  for (var x = 0; x <= width; x += 50) {
+    for (var y = 0; y <= height; y += 50){
       fill(random(255),0,random(255));
       ellipse(x ,y,25,25);
   
